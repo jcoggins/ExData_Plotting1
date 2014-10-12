115400334d01c38bbb051c58f197870cabcb9e14
jcoggins <javaisforjoe@gmail.com>
2014-10-12 19:11:02 -0400

    update plot4

--- plot4.R
+++ plot4.R
@@ -16,6 +16,6 @@ with(hPower, {
   plot(DateTime, Sub_metering_1, type="l", ylab="Energy Sub Metering", col = "grey")
   points(hPower$DateTime, hPower$Sub_metering_2, type = "l", col = "red")
   points(hPower$DateTime, hPower$Sub_metering_3, type = "l", col = "blue")
-  legend("topright", lty = 1, bty = "n", cex=0.4, col = c("grey", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", lty = 1, col = c("grey", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(DateTime, Global_reactive_power, type = "l")})
 dev.off()
