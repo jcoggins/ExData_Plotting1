f1295386e5966f67ff74cb5004d17608dbb37f44
jcoggins <javaisforjoe@gmail.com>
2014-10-13 00:44:00 -0400

    update plot3

--- plot3.R
+++ plot3.R
@@ -9,8 +9,11 @@ colnames(hPower)<- c("Date", "Time", "Global_active_power", "Global_reactive_pow
 DateTime <- paste(hPower$Date, hPower$Time)
 hPower$DateTime <- strptime(DateTime, "%d/%m/%Y %H:%M:%S")
 png("plot3.png", width=480, height=480, units="px")
-with(hPower, plot(DateTime,Sub_metering_1, type="l", ylab = "Energy Sub Metering", col="grey"))
-points(hPower$DateTime, hPower$Sub_metering_2, type="l", col="red")
-points(hPower$DateTime, hPower$Sub_metering_3, type="l", col="blue")
-legend("topright", lty = 1, col =c("grey", "red", "blue"),legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(hPower, {
+ plot(DateTime, Sub_metering_1, type="l", ylab="Energy Sub Metering", col = "grey")
+  points(hPower$DateTime, hPower$Sub_metering_2, type = "l", col = "red")
+  points(hPower$DateTime, hPower$Sub_metering_3, type = "l", col = "blue")
+  legend("topright", lty = 1, col = c("grey", "red", "blue"), 
+         legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
 dev.off()
